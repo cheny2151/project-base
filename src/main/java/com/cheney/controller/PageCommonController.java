7607fb608485c9a147e33be99948a3d91a3a18d9
cheny2151 <cheny21512151@gmail.com>
2018-07-16 15:02:15 +0800

    update

--- src/main/java/com/cheney/controller/PageCommonController.java
+++ src/main/java/com/cheney/controller/PageCommonController.java
@@ -1,9 +1,20 @@
 package com.cheney.controller;
 
+import com.cheney.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
 
 @Controller("pageCommonController")
 public class PageCommonController {
 
+    @Resource(name = "userServiceImpl")
+    private UserService userService;
+
+    @RequestMapping("/test")
+    public String test() {
+        return "/index.html";
+    }
 
 }
