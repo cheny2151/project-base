c709448ec9cc1637e0c62cd47c74986ec00bde29
cheny2151 <cheny21512151@gmail.com>
2018-09-08 11:36:39 +0800

    update

--- src/main/java/com/cheney/controller/PageCommonController.java
+++ src/main/java/com/cheney/controller/PageCommonController.java
@@ -2,7 +2,10 @@ package com.cheney.controller;
 
 import com.cheney.dao.mybatis.UserMapper;
 import com.cheney.entity.dto.AuthUser;
+import com.cheney.redis.RedisClient;
 import com.cheney.system.message.JsonMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,10 +18,16 @@ public class PageCommonController {
     @Resource(name = "userMapper")
     private UserMapper userMapper;
 
+    @Resource(name = "strRedisClient")
+    private RedisClient<String> redisClient;
+
+    @Autowired
+    private Environment environment;
+
     @RequestMapping("/test")
     @ResponseBody
     public JsonMessage test() {
-        AuthUser byUsername = userMapper.findByUsername("123");
+        AuthUser byUsername = userMapper.findByUsername("test");
         System.out.println(byUsername);
         return JsonMessage.success("123");
     }
