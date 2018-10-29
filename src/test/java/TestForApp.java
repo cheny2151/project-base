be2b6923e08d0c6d37922d2c9f60454c5637950e
cheney <478241123@qq.com>
2018-10-29 11:02:53 +0800

    [update]

--- src/test/java/TestForApp.java
+++ src/test/java/TestForApp.java
@@ -4,11 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Base64Utils;
+import sun.misc.BASE64Encoder;
 
 import javax.annotation.Resource;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = ApplicationContext.class)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = ApplicationContext.class)
 public class TestForApp {
 
     @Resource(name = "strRedisClient")
@@ -18,6 +20,14 @@ public class TestForApp {
     public void test() {
         redisClient.setValue("test", "success");
         System.out.println(redisClient.getValue("test"));
+    }
+
+    @Test
+    public void test2() {
+        byte[] encode = Base64Utils.encode("tradeAdmin:pw123456".getBytes());
+        System.out.println(new String(encode));
+        byte[] decode = Base64Utils.decodeFromString(new String(encode));
+        System.out.println(new String(decode));
     }
 
 }
