4969edb71fabce73064ff6d12c454ea38f6f903c
cheney <478241123@qq.com>
2018-11-28 10:28:41 +0800

    [update]

--- src/main/java/com/cheney/javaconfig/spring/TaskConfig.java
+++ src/main/java/com/cheney/javaconfig/spring/TaskConfig.java
@@ -6,8 +6,10 @@ import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 /**
  * 线程池配置
@@ -15,17 +17,31 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 @Configuration
 @EnableAsync
 @EnableScheduling
-public class TaskConfig {
+public class TaskConfig implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        scheduledTaskRegistrar.setTaskScheduler(taskScheduler());
+    }
 
     @Bean("taskScheduler")
-    public TaskScheduler fitTaskScheduler() {
+    public TaskScheduler taskScheduler() {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-        scheduler.setPoolSize(5);
+        //设置线程池容量
+        scheduler.setPoolSize(20);
+        //线程名前缀
+        scheduler.setThreadNamePrefix("taskScheduler-");
+        //等待时常
+        scheduler.setAwaitTerminationSeconds(60);
+        //当调度器shutdown被调用时等待当前被调度的任务完成
+        scheduler.setWaitForTasksToCompleteOnShutdown(true);
+        //设置当任务被取消的同时从当前调度器移除的策略
+        scheduler.setRemoveOnCancelPolicy(true);
         return scheduler;
     }
 
     @Bean("taskExecutor")
-    public TaskExecutor fitTaskExecutor() {
+    public TaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(5);
         executor.setMaxPoolSize(50);
