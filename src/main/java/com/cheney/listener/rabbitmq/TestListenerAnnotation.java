88722acf07eb5c85785943a3df3786c5466b0d12
cheney <478241123@qq.com>
2018-11-28 17:56:21 +0800

    [update]

---
+++ src/main/java/com/cheney/listener/rabbitmq/TestListenerAnnotation.java
@@ -0,0 +1,35 @@
+package com.cheney.listener.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+/**
+ * 注解方式消费消息队列
+ *
+ * @RabbitListener,@RabbitHandler
+ */
+@Slf4j
+@Component
+public class TestListenerAnnotation {
+
+    @RabbitListener(queues = "test_for_exchange")
+    public void onMessage(Message message, @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag, Channel channel) throws IOException {
+        try {
+            String body = new String(message.getBody(), Charset.forName("utf-8"));
+            log.info(Thread.currentThread().getId() + ":" + body);
+            channel.basicAck(deliveryTag, false);
+        } catch (Exception e) {
+            log.error("error", e);
+            channel.basicReject(deliveryTag, false);
+        }
+    }
+
+}
