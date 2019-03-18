00da006ae8c714f1f9cd28cab13d2c9abceea56b
cheney <478241123@qq.com>
2019-03-18 16:42:33 +0800

    [update]

--- src/main/java/com/cheney/system/page/Page.java
+++ src/main/java/com/cheney/system/page/Page.java
@@ -2,7 +2,6 @@ package com.cheney.system.page;
 
 import com.cheney.entity.dto.BaseEntity;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,43 +9,37 @@ import java.util.List;
  * 分页最外层(页面展示用)
  * Created by cheny on 2017/9/24.
  */
-public class Page<T> implements Serializable {
+public class Page<T> extends PageInfo {
 
     private static final long serialVersionUID = 285590709870311132L;
 
     private List<T> content;
 
-    private long total;
-
-    private int pageNumber;
-
-    private int pageSize;
-
     public Page() {
     }
 
     public Page(List<T> content, long total, PageInfo pageInfo) {
+        super();
+        super.setTotal(total);
+        super.setPageNumber(pageInfo.getPageNumber());
+        super.setPageSize(pageInfo.getPageSize());
+        if (total == 0) {
+            super.setTotalPage(0);
+        } else {
+            int pageSize = pageInfo.getPageSize();
+            super.setTotalPage(
+                    (int) ((total + pageSize - 1) / pageSize)
+            );
+        }
         this.content = content;
-        this.total = total;
-        this.pageNumber = pageInfo.getPageNumber();
-        this.pageSize = pageInfo.getPageSize();
     }
-
 
     public List<T> getContent() {
         return content;
     }
 
-    public long getTotal() {
-        return total;
-    }
-
-    public int getPageNumber() {
-        return pageNumber;
-    }
-
-    public int getPageSize() {
-        return pageSize;
+    public void setContent(List<T> content) {
+        this.content = content;
     }
 
     /**
