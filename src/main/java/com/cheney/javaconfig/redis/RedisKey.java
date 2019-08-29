341c8d01d3c9f57db1f0557810e5e80af8870d07
cheney <478241123@qq.com>
2019-08-29 09:31:13 +0800

    update

--- src/main/java/com/cheney/javaconfig/redis/RedisKey.java
+++ src/main/java/com/cheney/javaconfig/redis/RedisKey.java
@@ -1,7 +1,23 @@
 package com.cheney.javaconfig.redis;
 
-public class RedisKey {
+public enum RedisKey {
 
-    public final static String AUTH_TOKEN_KEY = "auth.details.%s";
+    AUTH_TOKEN_KEY("auth.details.%s");
+
+    private final static String KEY_PRE = "base";
+
+    private String key;
+
+    RedisKey(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return KEY_PRE + key;
+    }
+
+    public String getKey(String... format) {
+        return String.format(getKey(), (Object[]) format);
+    }
 
 }
