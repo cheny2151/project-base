c8aafc3850c44bc80830b1623d7cb00dd47264af
cheney <478241123@qq.com>
2019-09-03 19:27:11 +0800

    update

--- src/main/java/com/cheney/redis/clustertask/sub/test/TestSub.java
+++ src/main/java/com/cheney/redis/clustertask/sub/test/TestSub.java
@@ -1,7 +1,7 @@
 package com.cheney.redis.clustertask.sub.test;
 
 import com.cheney.redis.clustertask.TaskInfo;
-import com.cheney.redis.clustertask.pub.DefaultClusterTaskPublisher;
+import com.cheney.redis.clustertask.pub.ClusterTaskPublisher;
 import com.cheney.redis.clustertask.sub.AbstractClusterTaskSubscriber;
 import com.cheney.redis.clustertask.sub.SubTask;
 import com.cheney.system.page.Limit;
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
  * 继承AbstractClusterTaskSubscriber并存放于spring容器中
  * 再通过@SubTask指定执行的任务ID
  * <p>
- * (任务推送，调用{@link DefaultClusterTaskPublisher#publish(String, int, int, int)})
+ * (任务推送，调用{@link ClusterTaskPublisher#publish(String, int, int, int)})
  *
  * @author cheney
  * @date 2019-09-03
