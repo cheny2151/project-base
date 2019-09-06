385df19a899b74ff9382e4bdb42b5a8c1b7ad7f7
cheney <478241123@qq.com>
2019-09-06 14:40:12 +0800

    update

--- src/main/java/com/cheney/redis/RedisEval.java
+++ src/main/java/com/cheney/redis/RedisEval.java
@@ -2,10 +2,10 @@ package com.cheney.redis;
 
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.script.RedisScript;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisCluster;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -17,6 +17,8 @@ import java.util.List;
 public interface RedisEval {
 
     default Object execute(RedisTemplate redisTemplate, String script, List<String> keys, List<String> args) {
+        final List<String> finalKeys = keys == null ? Collections.emptyList() : keys;
+        final List<String> finalArgs = args == null ? Collections.emptyList() : args;
         return redisTemplate.execute((RedisCallback<Object>) (redisConnection) -> {
             Object nativeConnection = redisConnection.getNativeConnection();
 
@@ -24,11 +26,11 @@ public interface RedisEval {
 
             // 集群模式
             if (nativeConnection instanceof JedisCluster) {
-                result = ((JedisCluster) nativeConnection).eval(script, keys, args);
+                result = ((JedisCluster) nativeConnection).eval(script, finalKeys, finalArgs);
             }
             // 单机模式
             else if (nativeConnection instanceof Jedis) {
-                result = ((Jedis) nativeConnection).eval(script, keys, args);
+                result = ((Jedis) nativeConnection).eval(script, finalKeys, finalArgs);
             }
 
             return result;
