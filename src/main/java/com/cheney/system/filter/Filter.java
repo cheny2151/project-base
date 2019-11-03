cb8074d305885e5e91d8a840133111ecf831aaeb
cheney <478241123@qq.com>
2019-11-03 15:55:56 +0800

    update

--- src/main/java/com/cheney/system/filter/Filter.java
+++ src/main/java/com/cheney/system/filter/Filter.java
@@ -1,6 +1,12 @@
 package com.cheney.system.filter;
 
+import com.cheney.utils.PropertyNameUtils;
+import org.apache.commons.lang.StringUtils;
+
 public class Filter {
+
+    // 转为下划线
+    private static boolean USE_UNDERLINE = true;
 
     private String symbol;
 
@@ -31,6 +37,9 @@ public class Filter {
     }
 
     public String getProperty() {
+        if (USE_UNDERLINE && StringUtils.isNotEmpty(property)) {
+            property = PropertyNameUtils.underline(property);
+        }
         return property;
     }
 
