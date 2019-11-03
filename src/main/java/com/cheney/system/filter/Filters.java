635a67fd6cd11107c897e967397dc2fce454f188
cheney <478241123@qq.com>
2019-11-03 15:47:39 +0800

    update

--- src/main/java/com/cheney/system/filter/Filters.java
+++ src/main/java/com/cheney/system/filter/Filters.java
@@ -1,8 +1,44 @@
 package com.cheney.system.filter;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class Filters extends ArrayList<Filter> {
+
+    public enum Operator {
+
+        EQ("="),
+
+        NE("!="),
+
+        GT(">"),
+
+        LT("<"),
+
+        GE(">="),
+
+        LE("<="),
+
+        LIKE("like"),
+
+        NO_LIKE("not like"),
+
+        IN("in"),
+
+        IS_NULL("is null"),
+
+        IS_NOT_NULL("is not null");
+
+        private String script;
+
+        Operator(String script) {
+            this.script = script;
+        }
+
+        public String getScript() {
+            return script;
+        }
+    }
 
     public Filters() {
     }
@@ -15,6 +51,62 @@ public class Filters extends ArrayList<Filter> {
     public Filters addFilter(Filter filter) {
         this.add(filter);
         return this;
+    }
+
+    public static Filter eq(String property, Object value) {
+        return new EqualFilter(property, value);
+    }
+
+    public static Filter notEq(String property, Object value) {
+        return new NotEqualFilter(property, value);
+    }
+
+    public static Filter gt(String property, Object value) {
+        return new GreaterThanFilter(property, value);
+    }
+
+    public static Filter ge(String property, Object value) {
+        return new GreaterThanOrEqualFilter(property, value);
+    }
+
+    public static Filter lt(String property, Object value) {
+        return new LessThanFilter(property, value);
+    }
+
+    public static Filter le(String property, Object value) {
+        return new LessThanOrEqualFilter(property, value);
+    }
+
+    public static <T> Filter in(String property, Collection<T> value) {
+        return new InFilter(property, value);
+    }
+
+    public static <T> Filter in(String property, T[] value) {
+        return new InFilter(property, value);
+    }
+
+    public static Filter like(String property, Object value) {
+        return new LikeFilter(property, value);
+    }
+
+    public static Filter notLike(String property, Object value) {
+        return new NotLikeFilter(property, value);
+    }
+
+    public static Filter isNull(String property) {
+        return new NullFilter(property);
+    }
+
+    public static Filter isNotNull(String property) {
+        return new NotNullFilter(property);
+    }
+
+    public static Filter isNotLike(String property, Object value) {
+        return new NotLikeFilter(property, value);
+    }
+
+    public static Filters create(Filter filters) {
+        return new Filters(filters);
     }
 
 }
