2e9564f9f6a214c9dd07353fd042767f6a5dd317
cheney <478241123@qq.com>
2019-12-12 16:58:47 +0800

    update

--- src/main/java/com/cheney/utils/DateUtils.java
+++ src/main/java/com/cheney/utils/DateUtils.java
@@ -1,33 +1,57 @@
 package com.cheney.utils;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@Slf4j
 public class DateUtils {
 
     private final static String[] FORMATSTR = new String[]{"yyyy", "yyyy-MM", "yyyyMM", "yyyy/MM", "yyyy-MM-dd", "yyyyMMdd"
             , "yyyy/MM/dd", "yyyy-MM-dd HH:mm:ss", "yyyyMMddHHmmss", "yyyy/MM/dd HH:mm:ss"};
 
-    private final static SimpleDateFormat DEFAULT_FORMAT;
+    private final static ThreadLocal<SimpleDateFormat> DEFAULT_FORMAT = new ThreadLocal<>();
 
-    private final static SimpleDateFormat DAY_FORMAT;
-
-    static {
-        DEFAULT_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        DAY_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
-    }
+    private final static ThreadLocal<SimpleDateFormat> DAY_FORMAT = new ThreadLocal<>();
 
     public static Date parseDate(String date) throws ParseException {
         return org.apache.commons.lang.time.DateUtils.parseDate(date, FORMATSTR);
     }
 
     public static String formatDate(Date date) {
-        return DEFAULT_FORMAT.format(date);
+        return getDateFormat(DEFAULT_FORMAT, "yyyy-MM-dd HH:mm:ss").format(date);
     }
 
     public static String formatDay(Date date) {
-        return DAY_FORMAT.format(date);
+        return getDateFormat(DAY_FORMAT, "yyyy-MM-dd").format(date);
+    }
+
+    public static Date toDate(Object time) {
+        if (time instanceof String) {
+            try {
+                return org.apache.commons.lang3.time.DateUtils.parseDate((String) time, FORMATSTR);
+            } catch (ParseException e) {
+                log.error("时间解析错误", e);
+            }
+        }
+        if (time instanceof Date) {
+            return (Date) time;
+        }
+        if (time instanceof Long) {
+            return new Date((Long) time);
+        }
+        throw new IllegalArgumentException();
+    }
+
+
+    private static SimpleDateFormat getDateFormat(ThreadLocal<SimpleDateFormat> formatThreadLocal, String pattern) {
+        SimpleDateFormat simpleDateFormat = formatThreadLocal.get();
+        if (simpleDateFormat == null) {
+            formatThreadLocal.set(simpleDateFormat = new SimpleDateFormat(pattern));
+        }
+        return simpleDateFormat;
     }
 
 }
