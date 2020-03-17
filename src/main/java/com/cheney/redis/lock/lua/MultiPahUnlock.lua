80d959fe14932f8d61444745ed8099f7b287c809
cheney <478241123@qq.com>
2020-03-17 21:56:01 +0800

    update MultiPahUnlock

--- src/main/java/com/cheney/redis/lock/lua/MultiPahUnlock.lua
+++ src/main/java/com/cheney/redis/lock/lua/MultiPahUnlock.lua
@@ -6,18 +6,15 @@
 --
 
 -- 多路径锁解锁脚本
--- keys:1,锁Path;2,路径set的key;3,channel路径
+-- keys:1,路径set的key;2,channel路径
 -- ARGV:1,channel信息;剩余的为路径值，需将路径值存放到set中
 -- 返回：1,代表目前路径解锁成功/已经解锁;null代表未持有该锁
 if (redis.call('exists', KEYS[1]) == 0) then
-    redis.call('publish', KEYS[3], ARGV[1]);
+    redis.call('publish', KEYS[2], ARGV[1]);
     return 1;
 end
 for i = 2, #ARGV do
-    redis.call('srem', KEYS[2], ARGV[i])
+    redis.call('srem', KEYS[1], ARGV[i])
 end
-if (redis.call('exists', KEYS[2]) == 0) then
-    redis.call('del', KEYS[1]);
-end
-redis.call('publish', KEYS[3], ARGV[1]);
+redis.call('publish', KEYS[2], ARGV[1]);
 return 1;
--- src/main/java/com/cheney/redis/lock/lua/MultiPathLock.lua
+++ src/main/java/com/cheney/redis/lock/lua/MultiPathLock.lua
@@ -6,35 +6,32 @@
 --
 
 -- 多路径锁上锁脚本
--- keys:1,锁Path;2,路径set的key
+-- keys:1,路径set的key
 -- ARGV:1,过期时间;剩余的为路径值，需将路径值存放到set中
 -- 返回：null代表上锁成功;数值为过期时间
 if (redis.call('exists', KEYS[1]) == 0) then
-    redis.call('hset', KEYS[1], 'SET', KEYS[2]);
     for i = 2, #ARGV do
-        redis.call('sadd', KEYS[2], ARGV[i]);
+        redis.call('sadd', KEYS[1], ARGV[i]);
     end
     if (tonumber(ARGV[1]) > 0) then
         redis.call('pexpire', KEYS[1], ARGV[1]);
-        redis.call('pexpire', KEYS[2], ARGV[1]);
     end
     return nil;
 end
 
 local ex = 0;
 for i = 2, #ARGV do
-    if (redis.call('sismember', KEYS[2], ARGV[i]) == 1) then
+    if (redis.call('sismember', KEYS[1], ARGV[i]) == 1) then
         ex = 1;
         break;
     end
 end
 if (ex == 0) then
     for i = 2, #ARGV do
-        redis.call('sadd', KEYS[2], ARGV[i]);
+        redis.call('sadd', KEYS[1], ARGV[i]);
     end
     if (tonumber(ARGV[1]) > 0) then
         redis.call('pexpire', KEYS[1], ARGV[1]);
-        redis.call('pexpire', KEYS[2], ARGV[1]);
     end
     return nil;
 else
