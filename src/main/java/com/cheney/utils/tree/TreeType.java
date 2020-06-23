38047b51afee269693fddc0fecf9ffdfdb9b83c4
cheney <478241123@qq.com>
2020-06-23 19:31:12 +0800

    update

--- src/main/java/com/cheney/utils/tree/TreeType.java
+++ src/main/java/com/cheney/utils/tree/TreeType.java
@@ -3,6 +3,7 @@ package com.cheney.utils.tree;
 import lombok.Data;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * @author cheney
@@ -20,5 +21,49 @@ public class TreeType<T> {
     private T parent;
 
     private Collection<T> children;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCodeSequence() {
+        return codeSequence;
+    }
+
+    public void setCodeSequence(String codeSequence) {
+        this.codeSequence = codeSequence;
+    }
+
+    public T getParent() {
+        return parent;
+    }
+
+    public void setParent(T parent) {
+        this.parent = parent;
+    }
+
+    public Collection<T> getChildren() {
+        return children;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TreeType<?> treeType = (TreeType<?>) o;
+
+        return Objects.equals(codeSequence, treeType.codeSequence);
+    }
+
+    @Override
+    public int hashCode() {
+        return codeSequence != null ? codeSequence.hashCode() : 0;
+    }
 
 }
