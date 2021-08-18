e169b6f42a002fa1a58b2bb4dde1c566564ff310
chenyi5 <chenyi5@yy.com>
2021-08-18 11:18:04 +0800

    KafkaConfig update

--- src/main/java/com/cheney/javaconfig/mq/KafkaConfig.java
+++ src/main/java/com/cheney/javaconfig/mq/KafkaConfig.java
@@ -1,15 +1,18 @@
 package com.cheney.javaconfig.mq;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.support.LoggingProducerListener;
 import org.springframework.kafka.support.ProducerListener;
+
+import java.util.Map;
 
 /**
  * @author cheney
@@ -25,20 +28,49 @@ public class KafkaConfig {
         this.properties = properties;
     }
 
-    @Bean("strKafkaTemplate")
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        KafkaTemplate<String, String> kafkaTemplate = new KafkaTemplate<>(kafkaProducerFactory());
-        kafkaTemplate.setProducerListener(kafkaProducerListener());
+    /**
+     * 单独配置一个自定义消费者工厂
+     * 此处设置enable.auto.commit=true
+     */
+    @Bean
+    public DefaultKafkaConsumerFactory<String, String> manualConsumerFactory() {
+        Map<String, Object> consumerProperties = properties.buildConsumerProperties();
+        consumerProperties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        return new DefaultKafkaConsumerFactory<>(consumerProperties);
+    }
+
+    /**
+     * 单独配置一个自定义消费者工厂
+     * 此处设置AckMode.MANUAL
+     */
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> manualKafkaListenerContainerFactory(@Qualifier("manualConsumerFactory") ConsumerFactory<String, String> manualConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(manualConsumerFactory);
+        ContainerProperties containerProperties = factory.getContainerProperties();
+        containerProperties.setAckMode(ContainerProperties.AckMode.MANUAL);
+        factory.setBatchListener(true);
+        return factory;
+    }
+
+    /**
+     * 单独配置一个自定的生产者
+     */
+    @Bean("myKafkaTemplate")
+    public KafkaTemplate<String, String> kafkaTemplate(@Qualifier("loggingKafkaProducerListener") ProducerListener<String, String> producerListener,
+                                                       @Qualifier("myKafkaProducerFactory") ProducerFactory<String, String> producerFactory) {
+        KafkaTemplate<String, String> kafkaTemplate = new KafkaTemplate<>(producerFactory);
+        kafkaTemplate.setProducerListener(producerListener);
         return kafkaTemplate;
     }
 
     @Bean
-    public ProducerListener<String, String> kafkaProducerListener() {
+    public ProducerListener<String, String> loggingKafkaProducerListener() {
         return new LoggingProducerListener<>();
     }
 
     @Bean
-    public ProducerFactory<String, String> kafkaProducerFactory() {
+    public ProducerFactory<String, String> myKafkaProducerFactory() {
         return new DefaultKafkaProducerFactory<>(this.properties.buildProducerProperties());
     }
 
