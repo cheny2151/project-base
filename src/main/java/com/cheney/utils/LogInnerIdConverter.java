1a487f4d23cd48f10dd9ecaa5234e5c2a753c42a
chenyi5 <chenyi5@yy.com>
2021-10-08 10:29:36 +0800

    update

---
+++ src/main/java/com/cheney/utils/LogInnerIdConverter.java
@@ -0,0 +1,19 @@
+package com.cheney.utils;
+/*
+import ch.qos.logback.classic.pattern.MessageConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+*//**
+ * logback自定义字段配置类
+ *
+ * @author by chenyi
+ * @date 2021/9/28
+ *//*
+public class LogInnerIdConverter extends MessageConverter {
+
+    @Override
+    public String convert(ILoggingEvent event) {
+        String innerId = RequestParamHolder.getInnerId();
+        return innerId == null ? " " : innerId;
+    }
+}*/
