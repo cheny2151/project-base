c89882849f13181e281838eefc0726082cc622c6
chenyi5 <chenyi5@joyy.com>
2021-11-04 14:44:41 +0800

    优化HttpSupport.java

--- src/main/java/com/cheney/utils/HttpSupport.java
+++ src/main/java/com/cheney/utils/HttpSupport.java
@@ -4,7 +4,17 @@ public class HttpSupport {
 
     public class ContentType {
 
+        public final static String KEY = "Content-Type";
+
         public final static String APPLICATION_JSON = "application/json";
+
+    }
+
+    public class ContentEncoding {
+
+        private final static String KEY = "Content-Encoding";
+
+        private final static String HEADER_GZIP = "gzip";
 
     }
 
